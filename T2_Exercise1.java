53146a9f18dbc40a9054affd61e06aba3cfe2403
Peiiyong <165624659+Peiiyong@users.noreply.github.com>
2024-11-30 10:59:06 +0800

    update1

--- T2_Exercise1.java
+++ T2_Exercise1.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class T2_Exercise1 {
-    
     /*
     Create a new private and public classes that can get at least 3 cars, model, make and price from the terminal, 
     and then calculate the average prices of the cars and show it in the terminal.
@@ -17,25 +16,26 @@ public class T2_Exercise1 {
     }
 
     public static void main(String[] args) {
-        double totalprice =0;
+        double totalprice = 0;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter a number:");
         int num = sc.nextInt();
 
-        for(int i=0;i<num;i++){
-            System.out.println("Enter the model of car " + (i+1) + ":");
-            String model=sc.next()+sc.nextLine();
-            System.out.println("Enter the make of car " + (i+1) + ":");
-            String make= sc.nextLine();
-            System.out.println("Enter the price of car " + (i+1) + ":");
-            double price = sc.nextDouble();
-            
-            T2_Exercise1 car = new T2_Exercise1(model, make, price);
-            totalprice+= car.price;
+        if(num>=3){
+            for(int i=0;i<num;i++){
+                System.out.println("Enter the model of car " + (i+1) + ":");
+                String model=sc.next()+sc.nextLine();
+                System.out.println("Enter the make of car " + (i+1) + ":");
+                String make= sc.nextLine();
+                System.out.println("Enter the price of car " + (i+1) + ":");
+                double price = sc.nextDouble();
+                
+                T2_Exercise1 car = new T2_Exercise1(model, make, price);
+                totalprice+= car.price;
+            }
+            double average = totalprice/num;
+            System.out.println("The average price is " + average);
         }
-
-        double average = totalprice/num;
-        System.out.println("The average price is " + average);
       }
 }
