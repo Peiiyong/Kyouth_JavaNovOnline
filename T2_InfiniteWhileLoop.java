7f48824593bf82dba99d320d66c7822e2fc91c1c
Peiiyong <165624659+Peiiyong@users.noreply.github.com>
2024-11-30 12:45:18 +0800

    update

---
+++ T2_InfiniteWhileLoop.java
@@ -0,0 +1,11 @@
+public class T2_InfiniteWhileLoop {
+    public static void main(String[] args) {
+        int counter =1;
+        while(counter <=5){
+            System.out.println("Counter:" +counter);
+            counter--; // continue run if the condition is <=5
+
+            break;//STOP the loop after finish first loop
+        }
+    }
+}
