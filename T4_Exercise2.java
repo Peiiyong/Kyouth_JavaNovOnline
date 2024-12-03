f3c4691d3b3942d25a409e1f7b045cb1fa95e963
Peiiyong <165624659+Peiiyong@users.noreply.github.com>
2024-12-03 15:20:10 +0800

    update

--- T4_Exercise2.java
+++ T4_Exercise2.java
@@ -20,8 +20,8 @@ public class T4_Exercise2 {
             dayName = "Monday";
             
             for(int i=0;i<matrix1.length;i++){
-                for(int j=0;j<matrix1[i].length;j++){
-                   System.out.print("Enter a 3*4 matrix: ");
+                for(int j=0;j<matrix1[i].length;j++){ //matrix1[i].length 表示第 i 行有多少列
+                   System.out.print("Enter a 3*4 matrix [" + i +"]["+j+"] :");
                     matrix1[i][j] = sc.next() + sc.nextLine(); 
                 }
             }
@@ -39,7 +39,7 @@ public class T4_Exercise2 {
 
             for(int i=0;i<matrix2.length;i++){
                 for(int j=0;j<matrix2[i].length;j++){
-                   System.out.print("Enter a 2*3 matrix: ");
+                   System.out.print("Enter a 2*3 matrix [" + i +"]["+j+"] :");
                     matrix2[i][j] = sc.next() + sc.nextLine(); 
                 }
             }
