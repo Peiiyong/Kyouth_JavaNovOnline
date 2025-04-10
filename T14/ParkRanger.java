0f43a03eff5a1e877353afdfe55c16a377453e42
Peiiyong <jemins2001@gmail.com>
2025-04-10 22:20:42 +0800

    update

---
+++ T14/ParkRanger.java
@@ -0,0 +1,12 @@
+public class ParkRanger { 
+    int birds = 10; 
+    public static void main ( String [] data ) { 
+        ParkRanger p=new ParkRanger();
+    int trees = 5; 
+    //float f2 = 5.0;
+    float f1=5f;
+    double f0=5f;
+    System . out . print ( trees + p.birds ) ; 
+    } 
+    } 
+   
